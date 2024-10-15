87a0aeced033a7d00be35c0cc1176098a384b2f4
Adriana Villela <50256412+avillela@users.noreply.github.com>
2024-10-15 17:24:08 -0400

    KinD in codespaces workaround (#10)
    * Workaround for KinD cluster failing to create in GH codespaces

--- README.md
+++ README.md
@@ -2,9 +2,6 @@
 
 This is the companion repo for Module 6 of the O'Reilly Observability Fundamentals video course.
 
-> 🚨🚨 **NOTE:** There is currently a bug that is preventing KinD from running in GitHub codespaces. To work around this, you can run this tutorial locally in VSCode using [dev containers](https://code.visualstudio.com/docs/devcontainers/tutorial). I have opened this issue with the KinD folks in the hopes of getting some sort of resolution: https://github.com/kubernetes-sigs/kind/issues/3748.
-
-
 ## References
 
 Course references can be found [here](./docs/references.md)
--- src/scripts/setup-with-k9s.sh
+++ src/scripts/setup-with-k9s.sh
@@ -5,13 +5,23 @@
 # ---------------------------
 
 # Install KinD
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.24.0/kind-linux-amd64
+[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.24.0/kind-linux-arm64
+
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 kind --version
 
-# Create KinD cluster
+# Create KinD cluster. See the following GitHub issue for more on why I had to resort to manually creating the docker network: https://github.com/kubernetes-sigs/kind/issues/3748
+docker network create -d=bridge -o com.docker.network.bridge.enable_ip_masquerade=true -o com.docker.network.driver.mtu=1500 --subnet fc00:f853:ccd:e793::/64 kind
 kind create cluster --name otel-python-lab
+
+# Install kubectl
+[ $(uname -m) = x86_64 ] && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+[ $(uname -m) = aarch64 ] && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
+
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
 
 # ---------------------------
 # -- Install VSCode extensions
